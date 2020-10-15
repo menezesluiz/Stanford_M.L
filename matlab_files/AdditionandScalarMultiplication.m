81afd1d5d0929dc1f87a322de6c34a8a9e1bab5a
Luiz Eduard <29684170+menezesluiz@users.noreply.github.com>
2020-10-15 15:14:03 -0300

    Stanford_M.L

--- matlab_files/AdditionandScalarMultiplication.m
+++ matlab_files/AdditionandScalarMultiplication.m
@@ -1,4 +1,4 @@
-% Initialize matrix A and B
+)% Initialize matrix A and B
 A = [1, 2, 4; 5, 3, 2]
 B = [1, 3, 4; 1, 1, 1]
 
---
+++ matlab_files/LinearAlgebra_Test.m
@@ -0,0 +1,37 @@
+% Exercise 1
+% Matrix A
+A = [4, 3; 6, 9]
+
+% Matrix B
+B = [-2, 9; -5, 2]
+
+% Sum A + B
+soma = A + B
+
+% Exercise 2
+x = [2; 7; 4; 1]
+
+% multiplication
+multi = 0.5 * x
+
+% Exercise 3
+u = [5; 1; 9]
+
+transpor = u.'
+
+% Exercise 4
+U = [3; -5; 4]
+
+V = [1; 2; 5]
+
+resultado = U.' * V
+
+% Exercise 5
+Z = [1, 2, 3]
+Y = [3, 4, 5]
+
+parcial1 = Z + Y
+parcial2 = Y + Z
+parcial3 = Z * Y * Z 
+parcial4 = Y * Z * Y
+
