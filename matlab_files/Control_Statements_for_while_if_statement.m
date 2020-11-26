37cb132979a62b6250b39e6775550b6d2d012380
Luiz Eduard <29684170+menezesluiz@users.noreply.github.com>
2020-11-26 16:47:40 -0300

    Stanford_M.L
    Stanford_M.L

--- matlab_files/Control_Statements_for_while_if_statement.m
+++ matlab_files/Control_Statements_for_while_if_statement.m
@@ -44,3 +44,31 @@ elseif v(1) == 2,
 else
     disp('The value is not one or two.');
 end;
+
+
+A = [1 2; 3 4; 5 6];
+B = [1 2 3; 4 5 6];
+
+C = A' + B;
+C
+C =  B * A;
+C
+C = B' + A;
+C
+
+A = [16 2 3 13; 5 11 10 8; 9 7 6 12; 4 14 15 1]
+A
+
+B = A(:, 1:2);
+B
+B = A(1:4, 1:2);
+B
+B = A(1:2, 1:4);
+B
+
+v = zeros(10, 1);
+for i = 1:10
+    for j = 1:10
+        v(i) = v(i) + A(i, j) * x(j);
+    end
+end
